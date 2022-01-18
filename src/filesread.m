9fa5d7fc6d5f33064635fb30473f9ee8a3c6fcc5
SarahAlidoost <f.alidoost@esciencecenter.nl>
2022-01-18 18:10:34 +0100

    define SoilPropertyPath InputPath OutputPath, remove unused variables, refactor save function in filesread

--- src/filesread.m
+++ src/filesread.m
@@ -1,23 +1,36 @@
+%%%%%%% Set paths %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Set soil parameters path
+SoilPropertyPath='/home/jovyan/shared/EcoExtreML/STEMMUS_SCOPEv1.0.0/input/SoilProperty/';
+% Set input paths including vegetation parameters and forcing dat files
+% Set this path the same as working dir and copy file there
+InputPath='/home/jovyan/shared/EcoExtreML/STEMMUS_SCOPEv1.0.0/input/';
+% Set output path
+OutputPath='/home/jovyan/shared/EcoExtreML/STEMMUS_SCOPEv1.0.0/output/';
+% Set forcing data path
+ForcingPath='/home/jovyan/shared/EcoExtreML/STEMMUS_SCOPEv1.0.0/Plumber2_data/';
+% Set forsing netcdf file name
+% here as an example, we pick 'FI-Hyy_1996-2014_FLUXNET2015_Met'
+ForcingFileName='FI-Hyy_1996-2014_FLUXNET2015_Met.nc';
+
+%%%%%%% Prepare input files. %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 global DELT IGBP_veg_long latitude longitude reference_height canopy_height sitename
-fileFolder=fullfile('../input/'); %read sitename
-dirOutput=dir(fullfile(fileFolder,'*.nc'));
+
+ForcingFilePath=fullfile(ForcingPath, ForcingFileName);
 %prepare input files
-Path ='../input/';
-%File=dir(fullfile(Path,'filename'));
-full_path=strcat(Path,dirOutput.name);
-sitefullname=dirOutput.name;
+sitefullname=dir(ForcingFilePath).name; %read sitename
     sitename=sitefullname(1:6);
     startyear=sitefullname(8:11);
     endyear=sitefullname(13:16);
     startyear=str2double(startyear);
     endyear=str2double(endyear);
-%ncFilePath='../input/';
 %ncdisp(sitefullname,'/','full');
 
-time1=ncread(full_path,'time');
+time1=ncread(ForcingFilePath,'time');
 t1=datenum(startyear,1,1,0,0,0);
 DELT=time1(2);
-Dur_tot=length(time1);
+%Set the end time of the main loop in STEMMUS_SCOPE.m
+Dur_tot=10;
+%Dur_tot=length(time1);
 dt=time1(2)/3600/24;
 t2=datenum(endyear,12,31,23,30,0);
 T=t1:dt:t2;
@@ -39,100 +52,100 @@ T8=T6-T7;
 time=T8;
 T10=year(T);
 
-RH=ncread(full_path,'RH');
+RH=ncread(ForcingFilePath,'RH');
 RHL=length(RH);
 RHa=reshape(RH,RHL,1);
 
-Tair=ncread(full_path,'Tair');
+Tair=ncread(ForcingFilePath,'Tair');
 TairL=length(Tair);
 Taira=reshape(Tair,TairL,1)-273.15;
 
 es= 6.107*10.^(Taira.*7.5./(237.3+Taira));
 ea=es.*RHa./100;
 
-SWdown=ncread(full_path,'SWdown');
+SWdown=ncread(ForcingFilePath,'SWdown');
 SWdownL=length(SWdown);
 SWdowna=reshape(SWdown,SWdownL,1);
 
 
-LWdown=ncread(full_path,'LWdown');
+LWdown=ncread(ForcingFilePath,'LWdown');
 LWdownL=length(LWdown);
 LWdowna=reshape(LWdown,LWdownL,1);
 
 
-VPD=ncread(full_path,'VPD');
+VPD=ncread(ForcingFilePath,'VPD');
 VPDL=length(VPD);
 VPDa=reshape(VPD,VPDL,1);
 
 
-Qair=ncread(full_path,'Qair');
+Qair=ncread(ForcingFilePath,'Qair');
 QairL=length(Qair);
 Qaira=reshape(Qair,QairL,1);
 
 
-Psurf=ncread(full_path,'Psurf');
+Psurf=ncread(ForcingFilePath,'Psurf');
 PsurfL=length(Psurf);
 Psurfa=reshape(Psurf,PsurfL,1);
 Psurfa=Psurfa./100;
 
 
-Precip=ncread(full_path,'Precip');
+Precip=ncread(ForcingFilePath,'Precip');
 PrecipL=length(Precip);
 Precipa=reshape(Precip,PrecipL,1);
 Precipa=Precipa./10;
 
 
-Wind=ncread(full_path,'Wind');
+Wind=ncread(ForcingFilePath,'Wind');
 WindL=length(Wind);
 Winda=reshape(Wind,WindL,1);
 
 
-CO2air=ncread(full_path,'CO2air');
+CO2air=ncread(ForcingFilePath,'CO2air');
 CO2airL=length(CO2air);
 CO2aira=reshape(CO2air,CO2airL,1);
 CO2aira=CO2aira.*44./22.4;
 
 
-latitude=ncread(full_path,'latitude');
-longitude=ncread(full_path,'longitude');
-elevation=ncread(full_path,'elevation');
+latitude=ncread(ForcingFilePath,'latitude');
+longitude=ncread(ForcingFilePath,'longitude');
+elevation=ncread(ForcingFilePath,'elevation');
 
-LAI=ncread(full_path,'LAI');
+LAI=ncread(ForcingFilePath,'LAI');
 LAIL=length(LAI);
 LAIa=reshape(LAI,LAIL,1);
 
 
-LAI_alternative=ncread(full_path,'LAI_alternative');
+LAI_alternative=ncread(ForcingFilePath,'LAI_alternative');
 LAI_alternativeL=length(LAI_alternative);
 LAI_alternativea=reshape(LAI_alternative,LAI_alternativeL,1);
 
-IGBP_veg_long=ncread(full_path,'IGBP_veg_long');
-reference_height=ncread(full_path,'reference_height');
-canopy_height=ncread(full_path,'canopy_height');
+IGBP_veg_long=ncread(ForcingFilePath,'IGBP_veg_long');
+reference_height=ncread(ForcingFilePath,'reference_height');
+canopy_height=ncread(ForcingFilePath,'canopy_height');
 % save .dat files for SCOPE
-%save ../input/LAI_alternative.dat -ascii LAI_alternative
-save ../input/t_.dat -ascii time
-save ../input/Ta_.dat -ascii Taira
-save ../input/Rin_.dat -ascii SWdowna
-save ../input/Rli_.dat -ascii LWdowna
-%save ../input/VPDa.dat -ascii VPDa
-%save ../input/Qaira.dat -ascii Qaira
-save ../input/p_.dat -ascii Psurfa
-%save ../input/Precipa.dat -ascii Precipa
-save ../input/u_.dat -ascii Winda
-%save ../input/RHa.dat -ascii RHa
-save ../input/CO2_.dat -ascii CO2aira
-%save ../input/latitude.dat -ascii latitude
-%save ../input/longitude.dat -ascii longitude
-%save ../input/reference_height.dat -ascii reference_height
-%save ../input/canopy_height.dat -ascii canopy_height
-%save ../input/elevation.dat -ascii elevation
-save ../input/ea_.dat -ascii ea
-save ../input/year_.dat -ascii T10
+%save([InputPath, 'LAI_alternative.dat'], '-ascii', LAI_alternative)
+save([InputPath, 't_.dat'], '-ascii', 'time')
+save([InputPath, 'Ta_.dat'], '-ascii', 'Taira')
+save([InputPath, 'Rin_.dat'], '-ascii', 'SWdowna')
+save([InputPath, 'Rli_.dat'], '-ascii', 'LWdowna')
+%save([InputPath, 'VPDa.dat'], '-ascii', 'VPDa')
+%save([InputPath, 'Qaira.dat'], '-ascii', 'Qaira')
+save([InputPath, 'p_.dat'], '-ascii', 'Psurfa')
+%save([InputPath, 'Precipa.dat'], '-ascii', 'Precipa') 
+save([InputPath, 'u_.dat'], '-ascii', 'Winda')
+%save([InputPath, 'RHa.dat'], '-ascii', 'RHa') 
+save([InputPath, 'CO2_.dat'], '-ascii', 'CO2aira') 
+%save([InputPath, 'latitude.dat'], '-ascii', 'latitude')
+%save([InputPath, 'longitude.dat'], '-ascii', 'longitude')
+%save([InputPath, 'reference_height.dat'], '-ascii', 'reference_height')
+%save([InputPath, 'canopy_height.dat'], '-ascii', 'canopy_height')
+%save([InputPath, 'elevation.dat'], '-ascii', 'elevation')
+save([InputPath, 'ea_.dat'], '-ascii', 'ea')
+save([InputPath, 'year_.dat'], '-ascii', 'T10')
 LAI=[time'; LAIa']';
-save ../input/LAI_.dat -ascii LAI %save meteorological data for STEMMUS
+save([InputPath, 'LAI_.dat'], '-ascii', 'LAI') %save meteorological data for STEMMUS
 Meteodata=[time';Taira';RHa';Winda';Psurfa';Precipa';SWdowna';LWdowna';VPDa';LAIa']'; 
-save ../input/Mdata.txt -ascii Meteodata %save meteorological data for STEMMUS
+save([InputPath, 'Mdata.txt'], '-ascii', 'Meteodata') %save meteorological data for STEMMUS
 %Lacationdata=[latitude;longitude;reference_height;canopy_height;elevation]';
-%save ../input/Lacationdata.txt -ascii Lacationdata
-clearvars -except DELT Dur_tot IGBP_veg_long latitude longitude reference_height canopy_height sitename
+%save([InputPath, 'Lacationdata.txt'], '-ascii', 'Lacationdata')
+clearvars -except SoilPropertyPath InputPath OutputPath DELT Dur_tot IGBP_veg_long latitude longitude reference_height canopy_height sitename

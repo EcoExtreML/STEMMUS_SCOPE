cea9d4e404e8c2cd2e6ec13bbbc5260b0faa9740
SarahAlidoost <f.alidoost@esciencecenter.nl>
2022-02-03 18:45:47 +0100

    remove global CFG from exe function

--- src/STEMMUS_SCOPE_exe.m
+++ src/STEMMUS_SCOPE_exe.m
@@ -1,6 +1,5 @@
 %%%%%%% A function to run STEMMUS_SCOPE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function STEMMUS_SCOPE_exe(config_file)
-global CFG
 CFG = config_file;
 
 % run STEMMUS_SCOPE main code

59422aeedb5394ef7d0dc41b31fce2408b205686
SarahAlidoost <55081872+SarahAlidoost@users.noreply.github.com>
2022-02-21 13:53:59 +0100

    Update src/+helpers/heaviside1.m
    Co-authored-by: Stefan Verhoeven <s.verhoeven@esciencecenter.nl>

--- src/+helpers/heaviside1.m
+++ src/+helpers/heaviside1.m
@@ -1,6 +1,6 @@
 function Y = heaviside1(X)
 %HEAVISIDE    Step function.
-% MATLAB heaviside function from toolbox symbolic isnot supported by MATLAB compiler.
+% MATLAB heaviside function from the symbolic toolbox is not supported by MATLAB compiler. 
 Y = zeros(size(X));
 Y(X > 0) = 1;
 Y(X == 0) = .5;

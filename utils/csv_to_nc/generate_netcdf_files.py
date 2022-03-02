83f2528800bc8064eeeea0b73398365273dcde35
SarahAlidoost <f.alidoost@esciencecenter.nl>
2022-03-02 17:07:34 +0100

    add a todo

--- utils/csv_to_nc/generate_netcdf_files.py
+++ utils/csv_to_nc/generate_netcdf_files.py
@@ -1,7 +1,8 @@
 
 import csv_to_nc
 
-# Forcing data
+# TODO convert it to cli
+# Edit path to forcing data
 forcing_path = './Plumber2_data'
 forcing_name = 'AR-SLu_2010-2010_FLUXNET2015_Met.nc'
 
@@ -11,10 +12,11 @@ csvfiles_path = './AR-SLu_2022-02-09-1551'
 # Generate EC data from forcing data
 ECdata_filename, lat, lon, nctime = csv_to_nc.generate_ec_data(forcing_path, forcing_name, csvfiles_path)
 
-# Generate netcdf file from model output csv files
+# Edit paths
 variables_filename = './Variables_will_be_in_NetCDF_file.csv'
 output_dir = './AR-SLu_2022-02-09-1551'
 
+# Generate netcdf file from model output csv files
 csv_to_nc.generateNetCdf(lat, lon, nctime, output_dir, csvfiles_path, variables_filename)
 
 

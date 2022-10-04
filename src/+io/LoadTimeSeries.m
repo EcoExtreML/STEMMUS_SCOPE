1452cdbb07480165a9162c284cf4646889ab6d54
SarahAlidoost <f.alidoost@esciencecenter.nl>
2022-10-04 15:21:33 +0200

    remove unused Dataset_dir, use fullfile for joining paths

--- src/+io/LoadTimeSeries.m
+++ src/+io/LoadTimeSeries.m
@@ -5,7 +5,6 @@ function [ScopeParameters,xyt,canopy]  = LoadTimeSeries(ScopeParameters,leafbio,
 
     %}
 
-    Dataset_dir         = '';
     t_file              = char(F(6).FileName);
     year_file           = char(F(7).FileName);
     Rin_file            = char(F(8).FileName);
@@ -24,15 +23,15 @@ function [ScopeParameters,xyt,canopy]  = LoadTimeSeries(ScopeParameters,leafbio,
     Cab_file            = char(F(21).FileName);
 
     %% 1. Time and zenith angle
-    xyt.t               = load([path_input,Dataset_dir,'/' ,t_file] );
-    xyt.year            = load([path_input,Dataset_dir,'/',year_file]);
+    xyt.t               = load(fullfile(path_input, t_file));
+    xyt.year            = load(fullfile(path_input, year_file));
     t_                  = xyt.t;
 
     DOY_                = floor(t_)+1;
     time_               = 24*(t_-floor(t_));
 
     if ~isempty(tts_file)
-        ScopeParameters(51).Val      = load([path_input,Dataset_dir,'/',tts_file]);
+        ScopeParameters(51).Val      = load(fullfile(path_input, tts_file));
     else
         ttsR            = equations.calczenithangle(DOY_,time_ - xyt.timezn ,0,0,xyt.LON,xyt.LAT);     %sun zenith angle in rad
         ScopeParameters(51).Val       = min(85,ttsR/constants.deg2rad);                         %sun zenith angle in deg
@@ -40,56 +39,56 @@ function [ScopeParameters,xyt,canopy]  = LoadTimeSeries(ScopeParameters,leafbio,
     
     %% 2. Radiation 
     if ~isempty(Rin_file)
-        ScopeParameters(30).Val           = load([path_input,Dataset_dir,'/',Rin_file]);
+        ScopeParameters(30).Val           = load(fullfile(path_input, Rin_file));
     else 
         ScopeParameters(30).Val       = ScopeParameters(30).Val*ones(size(t_));
     end
     if ~isempty(Rli_file)
-        ScopeParameters(32).Val           = load([path_input,Dataset_dir,'/',Rli_file]);
+        ScopeParameters(32).Val           = load(fullfile(path_input, Rli_file));
     else
         ScopeParameters(32).Val       = ScopeParameters(32).Val*ones(size(t_));
     end
 
     %% 3. Windspeed, air temperature, humidity and air pressure
     if ~isempty(u_file)% wind speed
-        ScopeParameters(35).Val           = load([path_input,Dataset_dir,'/',u_file]); 
+        ScopeParameters(35).Val           = load(fullfile(path_input, u_file)); 
     else
         ScopeParameters(35).Val           = ScopeParameters(35).Val*ones(size(t_));
     end
 
     if ~isempty(Ta_file)%air temperature
-        ScopeParameters(31).Val           = load([path_input,Dataset_dir,'/',Ta_file]);
+        ScopeParameters(31).Val           = load(fullfile(path_input, Ta_file));
     else
         ScopeParameters(31).Val           = ScopeParameters(31).Val*ones(size(t_));
     end
 
     if ~isempty(ea_file)%air temperature
-        ScopeParameters(34).Val           = load([path_input,Dataset_dir,'/',ea_file]);
+        ScopeParameters(34).Val           = load(fullfile(path_input, ea_file));
     else
         ScopeParameters(34).Val           = ScopeParameters(34).Val*ones(size(t_));
     end
 
     if ~isempty(p_file)
-        ScopeParameters(33).Val       = load([path_input,Dataset_dir,'/',p_file]);
+        ScopeParameters(33).Val       = load(fullfile(path_input, p_file));
     else
         ScopeParameters(33).Val       = ScopeParameters(33).Val*ones(size(t_));
     end
 
     %% 4. Vegetation structure (measurement height, vegetation height and LAI)
     if ~isempty(z_file)
-        ztable          = load([path_input,Dataset_dir,'/',z_file]);
+        ztable          = load(fullfile(path_input, z_file));
         ScopeParameters(29).Val       = interp1(ztable(:,1),ztable(:,2),t_);
     else
         ScopeParameters(29).Val       = meteo.z*ones(size(t_));
     end
     if  ~isempty(LAI_file)
-        LAItable        = load([path_input,Dataset_dir,'/',LAI_file]);
+        LAItable        = load(fullfile(path_input, LAI_file));
         ScopeParameters(22).Val       = LAItable(:,2);
     else
         ScopeParameters(22).Val          = canopy.LAI*ones(size(time_));
     end
     if  ~isempty(hc_file)
-        hctable         = load([path_input,Dataset_dir,'/',hc_file]);
+        hctable         = load(fullfile(path_input, hc_file));
         ScopeParameters(23).Val       = interp1(hctable(:,1),hctable(:,2),t_); 
         canopy.hc = ScopeParameters(23).Val;
         if options.calc_zo
@@ -108,7 +107,7 @@ function [ScopeParameters,xyt,canopy]  = LoadTimeSeries(ScopeParameters,leafbio,
 
     %% 5. Gas concentrations
     if ~isempty(CO2_file)
-        Ca_          = load([path_input,Dataset_dir,'/',CO2_file])*constants.Mair/constants.MCO2/constants.rhoa; % conversion from mg m-3 to ppm
+        Ca_          = load(fullfile(path_input, CO2_file))*constants.Mair/constants.MCO2/constants.rhoa; % conversion from mg m-3 to ppm
         % mg(CO2)/m-3 * g(air)/mol(air) * mol(CO2)/g(CO2) * m3(air)/kg(air) * 10^-3 g(CO2)/mg(CO2) * 10^-3 kg(air)/g(air) * 10^6 ppm  
         jj              = isnan(Ca_);                           %find data with good quality  Ca data
         Ca_(jj)      = 380;
@@ -119,19 +118,19 @@ function [ScopeParameters,xyt,canopy]  = LoadTimeSeries(ScopeParameters,leafbio,
 
     %% 6. Soil Moisture Content
     if ~isempty(SMC_file)
-        ScopeParameters(54).Val          = load([path_input,Dataset_dir,'/',SMC_file]);
+        ScopeParameters(54).Val          = load(fullfile(path_input, SMC_file));
     end
 
     %% 7. Leaf biochemical parameters
     if ~isempty(Vcmax_file)
-        Vcmaxtable      = load([path_input,Dataset_dir,'/',Vcmax_file]);
+        Vcmaxtable      = load(fullfile(path_input, Vcmax_file));
         ScopeParameters(9).Val         = interp1(Vcmaxtable(:,1),Vcmaxtable(:,2),t_);
     else
         ScopeParameters(9).Val        = leafbio.Vcmo*ones(size(t_));
     end
 
     if ~isempty(Cab_file)
-        Cabtable        = load([path_input,Dataset_dir,'/',Cab_file]);
+        Cabtable        = load(fullfile(path_input, Cab_file));
         ScopeParameters(1).Val         = interp1(Cabtable(:,1),Cabtable(:,2),t_);
     else
         ScopeParameters(1).Val        = leafbio.Cab*ones(size(t_));

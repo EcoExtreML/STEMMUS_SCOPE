815b4d04ffe7c08f5f6fde1aed5497332f14560c
SarahAlidoost <f.alidoost@esciencecenter.nl>
2022-10-28 11:14:45 +0200

    remove if statement in octave script

--- src/STEMMUS_SCOPE_octave.m
+++ src/STEMMUS_SCOPE_octave.m
@@ -2,12 +2,8 @@
 function STEMMUS_SCOPE_octave(config_file)
 
 % Load in required Octave packages if STEMMUS-SCOPE is being run in Octave:
-if exist('OCTAVE_VERSION', 'builtin') ~= 0
-    pkg load netcdf
-    pkg load statistics
-else
-    warning("Install Octave to run the model in Octave.");
-end
+pkg load netcdf
+pkg load statistics
 
 % disable all warnings 
 warning('off','all')

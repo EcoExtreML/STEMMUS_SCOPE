771ea8de3861302a1b3d1e5557a5d3563e65c44b
Yang <y.liu@esciencecenter.nl>
2022-10-31 10:49:11 +0100

    revise python script based on review

--- run_model_on_snellius/run_model.py
+++ run_model_on_snellius/run_model.py
@@ -1,15 +1,8 @@
-#!/usr/bin/env python
-"""
-2022.10.20      : Workflow operator for STEMMUS_SCOPE on Snellius
-Author          : Team Beta
-Date            : 2022.10.20
-Last Update     : 2022.10.25
-Description     : This script is used in the job submission file to run 
-                  StemmusScope model with PyStemmusScope on a HPC cluster
-                  using slurm workload manager.
-Dependancy      : PyStemmusScope
-Caveat          : In order to launch the model, users need to install
-                  PyStemmusScope from STEMMUS_SCOPE_Processing.
+"""Run STEMMUS_SCOPE on Snellius.
+This script is used in a job submission file to loop over forcing files and run
+StemmusScope model on a HPC cluster using slurm workload manager. In order to
+run the model, PyStemmusScope should be installed, see
+https://pystemmusscope.readthedocs.io/.
 """
 
 import argparse
@@ -28,58 +21,68 @@ def run_model(ncfile_index, job_id):
 
     # create an instance of the model
     model = StemmusScope(config_file=path_to_config_file, exe_file=path_to_exe_file)
-    
+
     # get the forcing file
-    forcing_filenames_list = [file.name for file in Path(model.config["ForcingPath"]).iterdir()]
+    forcing_filenames_list = [
+        file.name for file in Path(model.config["ForcingPath"]).iterdir()
+    ]
     # note that index starts from 1 from bash input, so ncfile_index-1
-    nc_file = forcing_filenames_list[ncfile_index-1]
+    nc_file = forcing_filenames_list[ncfile_index - 1]
     station_name = nc_file.split("_")[0]
 
-    # feed model with the correct forcing file 
-    config_path = model.setup(
-        ForcingFileName = nc_file,
-        NumberOfTimeSteps="10",
-    )
-
-    # create slurm log
-    slurm_log(ncfile_index, job_id, model.config, station_name)
+    # feed model with the correct forcing file
+    config_path = model.setup(ForcingFileName=nc_file)
 
     # run model
-    _ = model.run()
+    model_log = model.run()
 
     # save output in netcdf format
     required_netcdf_variables = "../utils/csv_to_nc/required_netcdf_variables.csv"
-    _ = save.to_netcdf(config_path, required_netcdf_variables)
+    netcdf_file_name = save.to_netcdf(config_path, required_netcdf_variables)
+
+    slurm_log_msg = [
+        f"Input path is {model.config['InputPath']}",
+        f"Output path is {model.config['OutputPath']}",
+        f"model log is {model_log}",
+        f"model outputs are in {netcdf_file_name}",
+    ]
+
+    slurm_file_name = (
+        Path(model.config["OutputPath"])
+        / f"slurm_{job_id}_{ncfile_index}_{station_name}.out"
+    )
+
+    # create slurm log
+    slurm_log(slurm_file_name, slurm_log_msg)
 
 
-def slurm_log(ncfile_index, job_id, config, station_name):
-    # list all paths for display
-    SoilPropertyPath = config["SoilPropertyPath"]
-    InputPath = config["InputPath"]
-    OutputPath = config["OutputPath"]
-    ForcingPath = config["ForcingPath"]
-    # get vegetation path from leafangles path
-    VegetationPropertyPath = config["leafangles"].split("/leafangles")[0]
-    WorkDir = config["WorkDir"]
-    ForcingFileName = config["ForcingFileName"]
-
-    with open(f"../slurm/slurm_{job_id}_{ncfile_index}_{station_name}.out",'w+',encoding = 'utf-8') as f:
-        f.write(f"SoilPropertyPath is {SoilPropertyPath}\n")
-        f.write(f"InputPath is {InputPath}\n")
-        f.write(f"OutputPath is {OutputPath}\n")
-        f.write(f"ForcingPath is {ForcingPath}\n")
-        f.write(f"VegetationPropertyPath is {VegetationPropertyPath}\n")
-        f.write(f"WorkDir is {WorkDir}\n")
-        f.write(f"ForcingFileName is {ForcingFileName}\n")
+def slurm_log(slurm_file_name, slurm_log_msg):
+    """Create slurm log file for the submitted job."""
+    with open(slurm_file_name, "w+", encoding="utf-8") as file:
+        for line in slurm_log_msg:
+            file.write(line)
+            file.write("\n")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-n', '--ncfile_index', required=True, type=int, default=0,
-                        help="index of netCDF file")
-    parser.add_argument('-j', '--job_id', required=True, type=int, default=0,
-                        help="slurm job id from snellius")    
+    parser.add_argument(
+        "-n",
+        "--ncfile_index",
+        required=True,
+        type=int,
+        default=0,
+        help="index of netCDF file",
+    )
+    parser.add_argument(
+        "-j",
+        "--job_id",
+        required=True,
+        type=int,
+        default=0,
+        help="slurm job id from snellius",
+    )
     # get arguments
     args = parser.parse_args()
 

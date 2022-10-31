ebf1abd3752c38dc91f35022522781107d842f02
Yang <y.liu@esciencecenter.nl>
2022-10-31 10:34:33 +0100

    update job submission file

--- run_model_on_snellius/run_stemmus_scope_snellius.sh
+++ run_model_on_snellius/run_stemmus_scope_snellius.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This is a batch script for Snellius at Surf
-# usage: cd STEMMUS_SCOPE; mkdir -p slurm; sbatch run_stemmus_scope_snellius.sh
+# usage: cd STEMMUS_SCOPE/run_model_on_snellius; sbatch run_stemmus_scope_snellius.sh
 
 # SLURM settings
 #SBATCH -J stemmus_scope
@@ -8,8 +8,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=32
 #SBATCH -p thin
-#SBATCH --output=../slurm/slurm_%j.out
-#SBATCH --error=../slurm/slurm_%j.out
+#SBATCH --output=./slurm_%j.out
+#SBATCH --error=./slurm_%j.out
 
 ### 1. Load module needed to run the model (no need for license)
 module load 2021
@@ -17,9 +17,9 @@ module load 2021
 ### This is for Matlab
 module load MCR/R2021a.3
 
-### python environment stemmus is needed to convert csv files to nc files
-### see utils/csv_to_nc/README.md
-. ~/mamba/bin/activate stemmus
+### python environment pystemmusscope is needed
+### see https://pystemmusscope.readthedocs.io
+. ~/mamba/bin/activate pystemmusscope
 
 ### 2. Some security: stop script on error and undefined variables
 set -euo pipefail

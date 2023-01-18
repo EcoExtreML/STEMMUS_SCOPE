c7693fdc26fc11883330959bfa1638938dbd6bb0
SarahAlidoost <f.alidoost@esciencecenter.nl>
2023-01-18 14:01:03 +0100

    fix equation of h in runSubroutine0

--- src/+init/runSubroutine0.m
+++ src/+init/runSubroutine0.m
@@ -22,7 +22,7 @@ function [SoilVariables, Genuchten, initH] = runSubroutine0(Dmark, SoilConstants
         delta2 = ML + 2 - Dmark; %TODO check if it is ML not NL
         domainZ = i - Dmark + 1;
         SoilVariables.T(i) = init.calcSoilTemp(initT(1), initT(2), delta1, domainZ);
-        SoilVariables.h(i) = init.calcSoilTemp(initT(1), initT(2), delta2, domainZ);
+        SoilVariables.h(i) = init.calcSoilTemp(initH(1), initH(2), delta2, domainZ);
         SoilVariables.IH(j) = 1;
     end
 

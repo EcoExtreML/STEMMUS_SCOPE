7e4da1f57064a0d4f9a2cbfca6df926c6aebcdc9
Bart Schilperoort <b.schilperoort@esciencecenter.nl>
2023-04-11 13:34:19 +0200

    Fix identation issues

--- src/h_sub.m
+++ src/h_sub.m
@@ -24,8 +24,8 @@ function h_sub
     global TT_CRIT T0 h_frez hh_frez Theta_UU Theta_U DTheta_UUh KfL_h Rn SAVEhh CORh r_a_SOIL Resis_a Precip_msr SUMTIME Gvc SAVEDTheta_UUh SAVEDTheta_LLh Rns
 
     [Chh, ChT, Khh, KhT, Kha, Vvh, VvT, Chg, DTheta_LLh, DTheta_LLT, DTheta_UUh, SAVEDTheta_UUh, SAVEDTheta_LLh] = hPARM(NL, hh, ...
-                                                                                                           h, TT, T, Theta_LL, Theta_L, DTheta_LLh, DTheta_LLT, RHOV, RHOL, Theta_V, V_A, Eta, DRHOVh, ...
-                                                                                                           DRHOVT, KfL_h, D_Ta, KL_T, D_V, D_Vg, COR, Beta_g, Gamma0, Gamma_w, KLa_Switch, DVa_Switch, hThmrl, Thmrlefc, nD, TT_CRIT, T0, h_frez, hh_frez, Theta_UU, Theta_U, CORh, DTheta_UUh, Chh, ChT, Khh, KhT);
+                                                                                                                         h, TT, T, Theta_LL, Theta_L, DTheta_LLh, DTheta_LLT, RHOV, RHOL, Theta_V, V_A, Eta, DRHOVh, ...
+                                                                                                                         DRHOVT, KfL_h, D_Ta, KL_T, D_V, D_Vg, COR, Beta_g, Gamma0, Gamma_w, KLa_Switch, DVa_Switch, hThmrl, Thmrlefc, nD, TT_CRIT, T0, h_frez, hh_frez, Theta_UU, Theta_U, CORh, DTheta_UUh, Chh, ChT, Khh, KhT);
     [C1, C2, C4, C3, C4_a, C5, C6, C7, C5_a, C9] = h_MAT(Chh, ChT, Khh, KhT, Kha, Vvh, VvT, Chg, DeltZ, NL, NN, Srt);
     [RHS, C4, SAVE] = h_EQ(C1, C2, C4, C5, C6, C7, C5_a, C9, NL, NN, Delt_t, T, TT, h, P_gg, Thmrlefc, Soilairefc);
     [AVAIL0, RHS, C4, C4_a, Rn_SOIL, Evap, EVAP, Trap, r_a_SOIL, Resis_a, Srt, Precip] = h_BC(RHS, NBCh, NBChB, BCh, BChB, hN, KT, Delt_t, DSTOR0, NBChh, TIME, h_SUR, C4, KL_h, NN, C4_a, DeltZ, RHOV, Ta, HR_a, U, Theta_LL, Ts, Rv, g, NL, hh, rwuef, Theta_UU, Rn, T, TT, Gvc, Rns, Srt, Precip_msr, SUMTIME); % h_BC;

43501d5fd74a8621d3f8690c0792bc693badc0e2
Yunfei-Wang1993 <y.wang-3@utwente.nl>
2023-04-25 07:34:48 +0200

    set lookup talbe for biochemical parameters

--- src/+parameters/setTempParameters.m
+++ src/+parameters/setTempParameters.m
@@ -1,96 +1,111 @@
-function [ScopeParameters] = setTempParameters(ScopeParameters, siteName, vegetationType)
+function [ScopeParameters] = setTempParameters(ScopeParameters, siteName, landcoverClass)
     %{
-        %Set temperature parameters for different vegetation type.
+        %Set temperature parameters for different landcover type.
     %}
     siteName = cellstr(siteName);
-    if strcmp(vegetationType(1:18)', 'Permanent Wetlands')
-        ScopeParameters.Tparam = [0.2 0.3 288 313 328]; % These are five parameters specifying the temperature response.
-        ScopeParameters.Vcmo = [120]; % Vcmax, maximum carboxylation capacity (at optimum temperature)
-        ScopeParameters.m = [9]; % Ball-Berry stomatal conductance parameter
-        ScopeParameters.Type = [0]; % Photochemical pathway: 0=C3, 1=C4
-        ScopeParameters.leafwidth = [0.05]; % leaf width
-    elseif strcmp(vegetationType(1:19)', 'Evergreen Broadleaf')
-        ScopeParameters.Tparam = [0.2 0.3 283 311 328];
-        ScopeParameters.Vcmo = [80];
-        ScopeParameters.m = [9];
-        ScopeParameters.Type = [0];
-        ScopeParameters.leafwidth = [0.05];
-    elseif strcmp(vegetationType(1:19)', 'Deciduous Broadleaf')
-        ScopeParameters.Tparam = [0.2 0.3 283 311 328];
-        ScopeParameters.Vcmo = [80];
-        ScopeParameters.m = [9];
-        ScopeParameters.Type = [0];
-        ScopeParameters.leafwidth = [0.05];
-    elseif strcmp(vegetationType(1:13)', 'Mixed Forests')
-        ScopeParameters.Tparam = [0.2 0.3 281 307 328];
-        ScopeParameters.Vcmo = [80];
-        ScopeParameters.m = [9];
-        ScopeParameters.Type = [0];
-        ScopeParameters.leafwidth = [0.04];
-    elseif strcmp(vegetationType(1:20)', 'Evergreen Needleleaf')
-        ScopeParameters.Tparam = [0.2 0.3 278 303 328];
-        ScopeParameters.Vcmo = [80];
-        ScopeParameters.m = [9];
-        ScopeParameters.Type = [0];
-        ScopeParameters.leafwidth = [0.01];
-    elseif strcmp(vegetationType(1:9)', 'Croplands')
+    landcovers = unique(landcoverClass) % where landcoverClass is an array like ["forest", "forest", "forest", "shrubland", ...]
+    for ii=(1:length(landcovers)
+        Vcmo, Tparam, m, Type, Rdparam, leafwidth = landcover_variables(landcovers(ii));
+        ScopeParameters.lcVcmo(ii) = Vcmo;
+        ScopeParameters.lcTparam(ii) = Tparam;
+        ScopeParameters.lcm(ii) = m;
+        ScopeParameters.lcType(ii) = Type;
+        ScopeParameters.lcRdparam(ii) = Rdparam;
+        ScopeParameters.lcleafwidth(ii) = leafwidth;
+    end
+    
+end
+
+%% lookup table for Vcmo, Tparam, m, Type, Rdparam, and leafwidth
+
+function [Vcmo, Tparam, m, Type, Rdparam, leafwidth] = landcover_variables(landCoverType)
+    if strcmp(landCoverType(1:18)', 'Permanent Wetlands')
+        Tparam = [0.2 0.3 288 313 328]; % These are five parameters specifying the temperature response.
+        Vcmo = [120]; % Vcmax, maximum carboxylation capacity (at optimum temperature)
+        m = [9]; % Ball-Berry stomatal conductance parameter
+        Type = [0]; % Photochemical pathway: 0=C3, 1=C4
+        leafwidth = [0.05]; % leaf width
+    elseif strcmp(landCoverType(1:19)', 'Evergreen Broadleaf')
+        Tparam = [0.2 0.3 283 311 328];
+        Vcmo = [80];
+        m = [9];
+        Type = [0];
+        leafwidth = [0.05];
+    elseif strcmp(landCoverType(1:19)', 'Deciduous Broadleaf')
+        Tparam = [0.2 0.3 283 311 328];
+        m = [9];
+        Type = [0];
+        leafwidth = [0.05];
+    elseif strcmp(landCoverType(1:13)', 'Mixed Forests')
+        Tparam = [0.2 0.3 281 307 328];
+        Vcmo = [80];
+        m = [9];
+        Type = [0];
+        leafwidth = [0.04];
+    elseif strcmp(landCoverType(1:20)', 'Evergreen Needleleaf')
+        Tparam = [0.2 0.3 278 303 328];
+        Vcmo = [80];
+        m = [9];
+        Type = [0];
+        leafwidth = [0.01];
+    elseif strcmp(landCoverType(1:9)', 'Croplands')
         if isequal(siteName, {'ES-ES2'}) || isequal(siteName, {'FR-Gri'}) || isequal(siteName, {'US-ARM'}) || isequal(siteName, {'US-Ne1'})
-            ScopeParameters.Tparam = [0.2 0.3 278 303 328];
-            ScopeParameters.Vcmo = [50];
-            ScopeParameters.m = [4];
-            ScopeParameters.Type = [1];
-            ScopeParameters.Rdparam = [0.025]; % Respiration = Rdparam*Vcmcax
-            ScopeParameters.leafwidth = [0.03];
+            Tparam = [0.2 0.3 278 303 328];
+            Vcmo = [50];
+            m = [4];
+            Type = [1];
+            Rdparam = [0.025]; % Respiration = Rdparam*Vcmcax
+            leafwidth = [0.03];
         else
-            ScopeParameters.Tparam = [0.2 0.3 278 303 328];
-            ScopeParameters.Vcmo = [120];
-            ScopeParameters.m = [9];
-            ScopeParameters.Type = [0];
-            ScopeParameters.leafwidth = [0.03];
+            Tparam = [0.2 0.3 278 303 328];
+            Vcmo = [120];
+            m = [9];
+            Type = [0];
+            leafwidth = [0.03];
         end
-    elseif strcmp(vegetationType(1:15)', 'Open Shrublands')
-        ScopeParameters.Tparam = [0.2 0.3 288 313 328];
-        ScopeParameters.Vcmo = [120];
-        ScopeParameters.m = [9];
-        ScopeParameters.Type = [0];
-        ScopeParameters.leafwidth = [0.05];
-    elseif strcmp(vegetationType(1:17)', 'Closed Shrublands')
-        ScopeParameters.Tparam = [0.2 0.3 288 313 328];
-        ScopeParameters.Vcmo = [80];
-        ScopeParameters.m = [9];
-        ScopeParameters.Type = [0];
-        ScopeParameters.leafwidth = [0.05];
-    elseif strcmp(vegetationType(1:8)', 'Savannas')
-        ScopeParameters.Tparam = [0.2 0.3 278 313 328];
-        ScopeParameters.Vcmo = [120];
-        ScopeParameters.m = [9];
-        ScopeParameters.Type = [0];
-        ScopeParameters.leafwidth = [0.05];
-    elseif strcmp(vegetationType(1:14)', 'Woody Savannas')
-        ScopeParameters.Tparam = [0.2 0.3 278 313 328];
-        ScopeParameters.Vcmo = [120];
-        ScopeParameters.m = [9];
-        ScopeParameters.Type = [0];
-        ScopeParameters.leafwidth = [0.03];
-    elseif strcmp(vegetationType(1:9)', 'Grassland')
-        ScopeParameters.Tparam = [0.2 0.3 288 303 328];
+    elseif strcmp(landCoverType(1:15)', 'Open Shrublands')
+        Tparam = [0.2 0.3 288 313 328];
+        Vcmo = [120];
+        m = [9];
+        Type = [0];
+        leafwidth = [0.05];
+    elseif strcmp(landCoverType(1:17)', 'Closed Shrublands')
+        Tparam = [0.2 0.3 288 313 328];
+        Vcmo = [80];
+        m = [9];
+        Type = [0];
+        leafwidth = [0.05];
+    elseif strcmp(landCoverType(1:8)', 'Savannas')
+        Tparam = [0.2 0.3 278 313 328];
+        Vcmo = [120];
+        m = [9];
+        Type = [0];
+        leafwidth = [0.05];
+    elseif strcmp(landCoverType(1:14)', 'Woody Savannas')
+        Tparam = [0.2 0.3 278 313 328];
+        Vcmo = [120];
+        m = [9];
+        Type = [0];
+        leafwidth = [0.03];
+    elseif strcmp(landCoverType(1:9)', 'Grassland')
+        Tparam = [0.2 0.3 288 303 328];
         if isequal(siteName, {'AR-SLu'}) || isequal(siteName, {'AU-Ync'}) || isequal(siteName, {'CH-Oe1'}) || isequal(siteName, {'DK-Lva'}) || isequal(siteName, {'US-AR1'}) || isequal(siteName, {'US-AR2'}) || isequal(siteName, {'US-Aud'}) || isequal(siteName, {'US-SRG'})
-            ScopeParameters.Vcmo = [120];
-            ScopeParameters.m = [4];
-            ScopeParameters.Type = [1];
-            ScopeParameters.Rdparam = [0.025];
+            Vcmo = [120];
+            m = [4];
+            Type = [1];
+            Rdparam = [0.025];
         else
-            ScopeParameters.Vcmo = [120];
-            ScopeParameters.m = [9];
-            ScopeParameters.Type = [0];
+            Vcmo = [120];
+            m = [9];
+            Type = [0];
         end
-        ScopeParameters.leafwidth = [0.02];
+        leafwidth = [0.02];
     else
-        ScopeParameters.Tparam = [0.2 0.3 288 313 328];
-        ScopeParameters.Vcmo = [80];
-        ScopeParameters.m = [9];
-        ScopeParameters.Type = [0];
-        ScopeParameters.leafwidth = [0.05];
-        warning('IGBP vegetation name unknown, "%s" is not recognized. ', vegetationType);
+        Tparam = [0.2 0.3 288 313 328];
+        Vcmo = [80];
+        m = [9];
+        Type = [0];
+        leafwidth = [0.05];
+        warning('landCover name unknown, "%s" is not recognized. ', landCoverType);
     end
 end

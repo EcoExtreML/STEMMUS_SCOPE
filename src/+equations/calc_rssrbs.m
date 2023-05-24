accf75488af8ff93bcd455fce6c8d9e3758c80fe
yijianzeng <yijianzeng@gmail.com>
2023-05-24 16:18:19 +0200

    Fix_Style_Issue

--- src/+equations/calc_rssrbs.m
+++ src/+equations/calc_rssrbs.m
@@ -1,13 +1,13 @@
 function [rss, rbs] = calc_rssrbs(SMC, LAI, rbs, ResidualMC, fieldMC)
-%{
-This function is to calculate surface resistance, following C. van der Tol
-et al. 2014 (BG). The emperical parameters "aa, 4.1, and 4.3" are
-user-defined. And these parameters can be calibrated during winter season
-when LAI is low.
-%}
+    %{
+    This function is to calculate surface resistance, following C. van der Tol
+    et al. 2014 (BG). The emperical parameters "aa, 4.1, and 4.3" are
+    user-defined. And these parameters can be calibrated during winter season
+    when LAI is low.
+    %}
     aa = 3.8;
     bb = 4.1;
     cc = 4.3;
-     
+
     rss = exp((aa + bb) - aa * (SMC - ResidualMC) / (fieldMC - ResidualMC));
     rbs            = rbs * LAI / cc;

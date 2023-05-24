1cc09a145f8ec04f719f157b8b013af9083df969
EntingTang <etang20@163.com>
2023-05-24 14:21:01 +0200

    Explain variables in calc_rssrbs.m

--- src/+equations/calc_rssrbs.m
+++ src/+equations/calc_rssrbs.m
@@ -1,5 +1,5 @@
 function [rss,rbs] = calc_rssrbs(SMC,LAI,rbs)
 global SaturatedMC ResidualMC fieldMC
 aa=3.8;
-rss = exp((aa+4.1)-aa*(SMC-ResidualMC(1))/(fieldMC(1)-ResidualMC(1)));
-rbs            = rbs*LAI/4.3;
+rss = exp((aa+4.1)-aa*(SMC-ResidualMC(1))/(fieldMC(1)-ResidualMC(1))); % rss: soil surface resistance [s m−1]; aa and 4.1 is a pre-defined input
+rbs            = rbs*LAI/4.3; % rbs: resistances for leaf boundary layer

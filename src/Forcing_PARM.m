cd963d3fa12c31401b732fea4844ee1853e2c806
SarahAlidoost <f.alidoost@esciencecenter.nl>
2023-06-20 09:45:39 +0200

    remove unused varibales

--- src/Forcing_PARM.m
+++ src/Forcing_PARM.m
@@ -1,7 +1,12 @@
-function Forcing_PARM
-    global Rn TIMEOLD DELT
+function InterpolateForcingData(ForcingData, SoilData)
+    %{
+        Reconstruct the driving forces e.g. Ta, RH, Wind, Pressure,
+        precipitation, etc. to be a continuous function of time via Fourier
+        transformation, e.g. through identifying the dominant frequency etc.
+    %}
+
+    global Rn TIMEOLD DELT Precip Gvc Tss
     global Ta Ts U HR_a SH Rns Rnl KT P_Va RHOV_A Rv TopPg h_SUR NBCT
-    global Ta_msr RH_msr Rn_msr WS_msr Pg_msr HourInput Rns_msr Precip Precip_msr LAI_msr Gvc Tss
 
     if TIMEOLD == KT
         Ta(KT) = 0;
@@ -18,18 +23,21 @@ function Forcing_PARM
     if NBCT == 1 && KT == 1
         Ts(1) = 0;
     end
-    HourInput = 1; % DELT=3600;
-    WS_msr(WS_msr < 0.05) = 0.05;
-    Ta(KT) = Ta_msr(KT);
-    HR_a(KT) = 0.01 .* (RH_msr(KT));
-    U(KT) = 100 .* (WS_msr(KT));
-    Rns(KT) = (Rns_msr(KT)) * 8.64 / 24 / 100 * 1;
-    TopPg(KT) = 100 .* (Pg_msr(KT));
-    Ts(KT) = Tss;
-    Rn(KT) = (Rn_msr(KT)) * 8.64 / 24 / 100 * 1;
-    Precip(KT) = Precip_msr(KT) * 0.1 / DELT;
-    Gvc(KT) = LAI_msr(KT);
 
-    P_Va(KT) = 0.611 * exp(17.27 * Ta(KT) / (Ta(KT) + 237.3)) * HR_a(KT);  % The atmospheric vapor pressure (KPa)  (1000Pa=1000.1000.g.100^-1.cm^-1.s^-2)
+    ForcingData.WS_msr(ForcingData.WS_msr < 0.05) = 0.05;
+    Ta(KT) = ForcingData.Ta_msr(KT);
+    HR_a(KT) = 0.01 .* (ForcingData.RH_msr(KT));
+    U(KT) = 100 .* (ForcingData.WS_msr(KT));
+    Rns(KT) = (ForcingData.Rns_msr(KT)) * 8.64 / 24 / 100 * 1;
+    TopPg(KT) = 100 .* (ForcingData.Pg_msr(KT));
+    Ts(KT) = SoilData.Tss;
+    Rn(KT) = (ForcingData.Rn_msr(KT)) * 8.64 / 24 / 100 * 1;
+    Precip(KT) = ForcingData.Precip_msr(KT) * 0.1 / DELT;
+    Gvc(KT) = ForcingData.LAI_msr(KT);
+
+    % The atmospheric vapor pressure (KPa)  (1000Pa=1000.1000.g.100^-1.cm^-1.s^-2)
+    P_Va(KT) = 0.611 * exp(17.27 * Ta(KT) / (Ta(KT) + 237.3)) * HR_a(KT);
 
     RHOV_A(KT) = P_Va(KT) * 1e4 / (Rv * (Ta(KT) + 273.15));
+
+end

93db339fca5deece40aab720ba2c630a2cc04e24
SarahAlidoost <f.alidoost@esciencecenter.nl>
2023-07-17 14:54:29 +0200

    refactor CondL_h

--- src/CondL_h.m
+++ src/CondL_h.m
@@ -5,12 +5,10 @@ function [SoilVariables] = calculateHydraulicConductivity(SoilVariables, VanGenu
     % load Constants
     Constants = io.define_constants();
 
-    TT = SoilVariables.TT;
     MU_W = InitialValues.MU_W;
     KL_h = SoilVariables.KL_h;
     KfL_h = SoilVariables.KfL_h;
     KfL_T = SoilVariables.KfL_T;
-    TT_CRIT = SoilVariables.TT_CRIT;
 
     Theta_LL = SoilVariables.Theta_LL;
     DTheta_LLh = SoilVariables.DTheta_LLh;
@@ -40,52 +38,64 @@ function [SoilVariables] = calculateHydraulicConductivity(SoilVariables, VanGenu
     end
 
     for i = 1:ModelSettings.NL
-        for ND = 1:2
-            i = i + ND - 1;
+        for j = 1:2 % ND
+            i = i + j - 1;
 
             % slice
-            SV = slice(SoilVariables, i, ND);
-            VG = slice(VanGenuchten, i, ND);
+            SV = slice(SoilVariables, i, j);
+            VG = slice(VanGenuchten, i, j);
 
-            Gama_hh(i) = conductivity.hydraulicConductivity.calculateGamma_hh(SV.hh);
-            Theta_m(i) = conductivity.hydraulicConductivity.calculateTheta_m(Gama_hh(i), VG);
+            Gama_hh = conductivity.hydraulicConductivity.calculateGamma_hh(SV.hh);
+            Theta_m = conductivity.hydraulicConductivity.calculateTheta_m(Gama_hh, VG);
 
-            Theta_UU(i, ND) = conductivity.hydraulicConductivity.calculateTheta_UU(Theta_m(i), Gama_hh(i), SV, VG);
-            Theta_II(i, ND) = conductivity.hydraulicConductivity.calculateTheta_II(SV.TT, SV.XCAP);
-            Theta_LL(i, ND) = conductivity.hydraulicConductivity.calculateTheta_LL(Theta_UU(i, ND), Theta_II(i, ND), Theta_m(i), SV, VG);
+            Theta_UU = conductivity.hydraulicConductivity.calculateTheta_UU(Theta_m, Gama_hh, SV, VG);
+            Theta_II = conductivity.hydraulicConductivity.calculateTheta_II(SV.TT, SV.XCAP);
+            Theta_LL = conductivity.hydraulicConductivity.calculateTheta_LL(Theta_UU, Theta_II, Theta_m, SV, VG);
             % TODO issue circular calculation
-            Theta_II(i, ND) = (Theta_UU(i, ND) - Theta_LL(i, ND)) * Constants.RHOL / Constants.RHOI;  % ice water content
+            Theta_II = (Theta_UU - Theta_LL) * Constants.RHOL / Constants.RHOI;  % ice water content
 
-            DTheta_UUh(i, ND) = conductivity.hydraulicConductivity.calculate_DTheta_UUh(Theta_UU(i, ND), Theta_m(i), Theta_LL(i, ND), Theta_L(i, ND), Gama_hh(i), SV, VG);
-            DTheta_LLh(i, ND) = conductivity.hydraulicConductivity.calcuulate_DTheta_LLh(DTheta_UUh(i, ND), Theta_m(i), Theta_UU(i, ND), Theta_LL(i, ND), Gama_hh(i), SV, VG);
-            Se(i, ND) = conductivity.hydraulicConductivity.calculateSe(Theta_LL(i, ND), Gama_hh(i), SV);
+            DTheta_UUh = conductivity.hydraulicConductivity.calculate_DTheta_UUh(Theta_UU, Theta_m, Theta_LL, Theta_L, Gama_hh, SV, VG);
+            DTheta_LLh = conductivity.hydraulicConductivity.calcuulate_DTheta_LLh(DTheta_UUh, Theta_m, Theta_UU, Theta_LL, Gama_hh, SV, VG);
+            Se = conductivity.hydraulicConductivity.calculateSe(Theta_LL, Gama_hh, SV);
 
-            if Theta_UU(i, ND) ~= 0
-                Ratio_ice(i, ND) = Constants.RHOI * Theta_II(i, ND) / (Constants.RHOL * Theta_UU(i, ND)); % ice ratio
+            if Theta_UU ~= 0
+                Ratio_ice = Constants.RHOI * Theta_II / (Constants.RHOL * Theta_UU); % ice ratio
             else
-                Ratio_ice(i, ND) = 0;
+                Ratio_ice = 0;
             end
             if KIT
-                if TT(i) < -20
-                    MU_W(i, ND) = 3.71e-2;
-                elseif TT(i) > 150
-                    MU_W(i, ND) = 1.81e-3;
+                % TODO exact calculation of MU_W happens in CondL_Tdisp
+                if SV.TT < -20
+                    MU_W = 3.71e-2;
+                elseif SV.TT > 150
+                    MU_W = 1.81e-3;
                 else
-                    MU_W(i, ND) = Constants.MU_W0 * exp(Constants.MU1 / (8.31441 * (TT(i) + 133.3)));
+                    MU_W = Constants.MU_W0 * exp(Constants.MU1 / (8.31441 * (SV.TT + 133.3)));
                 end
 
-                KL_h(i, ND) = calculateKL_h(MU_W(i, ND), Se(i, ND), m(i));
+                KL_h = calculateKL_h(MU_W, Se, VG.m);
 
-                if Gama_hh(i) ~= 1
-                    KfL_h(i, ND) = KL_h(i, ND) * 10^(-1 * Imped(i) * Ratio_ice(i, ND));  % hydraulic conductivity for freezing soil
+                if Gama_hh ~= 1
+                    KfL_h = KL_h * 10^(-1 * Imped(i) * Ratio_ice);  % hydraulic conductivity for freezing soil
                 else
-                    KfL_h(i, ND) = KL_h(i, ND) * 10^(-1 * Imped(i) * Ratio_ice(i, ND));  % hydraulic conductivity for freezing soil
+                    KfL_h = KL_h * 10^(-1 * Imped(i) * Ratio_ice);  % hydraulic conductivity for freezing soil
                 end
-                KfL_T(i, ND) = helpers.heaviside1(TT_CRIT(i) - (TT(i) + ModelSettings.T0)) * L_f * 1e4 / (Constants.g * (ModelSettings.T0));   % thermal consider for freezing soil
+                KfL_T = helpers.heaviside1(SV.TT_CRIT - (SV.TT + ModelSettings.T0)) * L_f * 1e4 / (Constants.g * (ModelSettings.T0));   % thermal consider for freezing soil
             else
-                KL_h(i, ND) = 0;
-                KfL_h(i, ND) = 0;
-                KfL_T(i, ND) = 0;
+                KL_h = 0;
+                KfL_h = 0;
+                KfL_T = 0;
             end
         end
+
+    SoilVariables.KL_h(i, j) = KL_h;
+    SoilVariables.KfL_h(i, j) = KfL_h;
+    SoilVariables.KfL_T(i, j) = KfL_T;
+    SoilVariables.Theta_LL(i, j) = Theta_LL;
+    SoilVariables.DTheta_LLh(i, j) = DTheta_LLh;
+    SoilVariables.Theta_II(i, j) = Theta_II;
+    SoilVariables.Theta_UU(i, j) = Theta_UU;
+    SoilVariables.DTheta_UUh(i, j) = DTheta_UUh;
+    SoilVariables.Se(i, j) = Se;
+    SoilVariables.Gama_hh(i) = Gama_hh;
     end

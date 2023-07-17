31c0d41c93e2351a95295e59b1de82f125efffde
SarahAlidoost <f.alidoost@esciencecenter.nl>
2023-07-17 14:56:31 +0200

    refactor CondL_h in UpdateSoilWaterContent

--- src/UpdateSoilWaterContent.m
+++ src/UpdateSoilWaterContent.m
@@ -3,90 +3,50 @@ function SoilVariables = UpdateSoilWaterContent(KIT, L_f, SoilVariables, VanGenu
         Update SoilWaterContent i.e. Theta_LL.
     %}
 
-    % these lines can be removed after issue 181
     % get model settings
     ModelSettings = io.getModelSettings();
 
-    NN = ModelSettings.NN;
-    NL = ModelSettings.NL;
-    SWCC = ModelSettings.SWCC;
-    Thmrlefc = ModelSettings.Thmrlefc;
-    T0 = ModelSettings.T0;
-    Tr = ModelSettings.Tr;
-    hThmrl = ModelSettings.hThmrl;
-    Hystrs = ModelSettings.Hystrs;
-
     Theta_s = VanGenuchten.Theta_s;
-    Theta_r = VanGenuchten.Theta_r;
-    Alpha = VanGenuchten.Alpha;
-    n = VanGenuchten.n;
-    m = VanGenuchten.m;
 
     POR = SoilVariables.POR;
-    h = SoilVariables.h;
-    Lamda = SoilVariables.Lamda;
-    Phi_s = SoilVariables.Phi_s;
-
-    h_frez = SoilVariables.h_frez;
-    hh_frez = SoilVariables.hh_frez;
     TT = SoilVariables.TT;
     hh = SoilVariables.hh;
     COR = SoilVariables.COR;
     CORh = SoilVariables.CORh;
     XWRE = SoilVariables.XWRE;
     IH = SoilVariables.IH;
-    Ks = SoilVariables.Ks;
-    Imped = SoilVariables.Imped;
-    XCAP = SoilVariables.XCAP;
 
-    Theta_L = SoilVariables.Theta_L;
-    Theta_LL = SoilVariables.Theta_LL;
-    Theta_V = SoilVariables.Theta_V;
-    Theta_g = SoilVariables.Theta_g;
-    Se = SoilVariables.Se;
-    KL_h = SoilVariables.KL_h;
-    DTheta_LLh = SoilVariables.DTheta_LLh;
-    KfL_T = SoilVariables.KfL_T;
-    Theta_II = SoilVariables.Theta_II;
-    Theta_I = SoilVariables.Theta_I;
-    Theta_UU = SoilVariables.Theta_UU;
-    Theta_U = SoilVariables.Theta_U;
-    TT_CRIT = SoilVariables.TT_CRIT;
-    KfL_h = SoilVariables.KfL_h;
-    DTheta_UUh = SoilVariables.DTheta_UUh;
-
-    % get Constants
-    Constants = io.define_constants();
-    g = Constants.g;
-    RHOI = Constants.RHOI;
-    RHOL = Constants.RHOL;
-
-    if hThmrl == 1
-        for MN = 1:NN
+    if ModelSettings.hThmrl == 1
+        for MN = 1:ModelSettings.NN
             CORh(MN) = 0.0068;
-            COR(MN) = exp(-1 * CORh(MN) * (TT(MN) - Tr)); % *COR21(MN)
+            COR(MN) = exp(-1 * CORh(MN) * (TT(MN) - ModelSettings.Tr)); % *COR21(MN)
 
             if COR(MN) == 0
                 COR(MN) = 1;
             end
         end
     else
-        for MN = 1:NN
+        for MN = 1:ModelSettings.NN
             COR(MN) = 1;
         end
     end
 
-    for MN = 1:NN
+    for MN = 1:ModelSettings.NN
         hhU(MN) = COR(MN) * hh(MN);
         hh(MN) = hhU(MN);
     end
-    [Theta_LL, Se, KfL_h, KfL_T, DTheta_LLh, hh, hh_frez, Theta_UU, DTheta_UUh, Theta_II, KL_h] = CondL_h(SoilVariables, Theta_r, Theta_s, Alpha, hh, hh_frez, h_frez, n, m, Ks, NL, Theta_L, h, KIT, TT, Thmrlefc, POR, SWCC, Theta_U, XCAP, Phi_s, RHOI, RHOL, Lamda, Imped, L_f, g, T0, TT_CRIT, Theta_II, KfL_h, KfL_T, KL_h, Theta_UU, Theta_LL, DTheta_LLh, DTheta_UUh, Se);
-    for MN = 1:NN
+    SoilVariables = conductivity.calculateHydraulicConductivity(SoilVariables, VanGenuchten, InitialValues, KIT, L_f);
+    Theta_LL = SoilVariables.Theta_LL;
+    DTheta_LLh = SoilVariables.DTheta_LLh;
+    Theta_UU = SoilVariables.Theta_UU;
+    Theta_II = SoilVariables.Theta_II;
+
+    for MN = 1:ModelSettings.NN
         hhU(MN) = hh(MN);
         hh(MN) = hhU(MN) / COR(MN);
     end
-    if Hystrs == 0
-        for i = 1:NL
+    if ModelSettings.Hystrs == 0
+        for i = 1:ModelSettings.NL
             for ND = 1:2
                 Theta_V(i, ND) = POR(i) - Theta_UU(i, ND); % -Theta_II(i,ND); % Theta_LL==>Theta_UU
                 if Theta_V(i, ND) <= 1e-14
@@ -96,7 +56,7 @@ function SoilVariables = UpdateSoilWaterContent(KIT, L_f, SoilVariables, VanGenu
             end
         end
     else
-        for i = 1:NL
+        for i = 1:ModelSettings.NL
             for ND = 1:2
                 if IH(i) == 2
                     if XWRE(i, ND) < Theta_LL(i, ND)
@@ -133,20 +93,9 @@ function SoilVariables = UpdateSoilWaterContent(KIT, L_f, SoilVariables, VanGenu
             end
         end
     end
-    SoilVariables.KfL_T = KfL_T;
-    SoilVariables.Theta_II = Theta_II;
-    SoilVariables.Theta_UU = Theta_UU;
-
-    SoilVariables.hh_frez = hh_frez;
     SoilVariables.hh = hh;
     SoilVariables.COR = COR;
     SoilVariables.CORh = CORh;
-    SoilVariables.Se = Se;
-    SoilVariables.KL_h = KL_h;
-    SoilVariables.Theta_LL = Theta_LL;
-    SoilVariables.DTheta_LLh = DTheta_LLh;
-    SoilVariables.KfL_h = KfL_h;
     SoilVariables.Theta_V = Theta_V;
     SoilVariables.Theta_g = Theta_g;
-    SoilVariables.DTheta_UUh = DTheta_UUh;
 end

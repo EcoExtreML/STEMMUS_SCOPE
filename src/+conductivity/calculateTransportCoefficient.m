e099a64ce9b5a53b95ca7d69ce9b41f1a8a42085
SarahAlidoost <f.alidoost@esciencecenter.nl>
2023-07-25 17:15:08 +0200

    polish calculateTransportCoefficient

--- src/+conductivity/calculateTransportCoefficient.m
+++ src/+conductivity/calculateTransportCoefficient.m
@@ -15,10 +15,10 @@ function TransportCoefficient = calculateTransportCoefficient(InitialValues, Soi
     % should be used. That is why "f0" is in the numerator. Note: f0 in L_WT
     % has been changed as 1.5. The Rv in MU_W should be 8.31441 i/mol.K.
 
-    W = InitialValues.W;
-    WW = InitialValues.WW;
-    MU_W = InitialValues.MU_W;
-    D_Ta = InitialValues.D_Ta;
+    TransportCoefficient.W = InitialValues.W;
+    TransportCoefficient.WW = InitialValues.WW;
+    TransportCoefficient.MU_W = InitialValues.MU_W;
+    TransportCoefficient.D_Ta = InitialValues.D_Ta;
 
     % get model settings
     ModelSettings = io.getModelSettings();
@@ -30,46 +30,45 @@ function TransportCoefficient = calculateTransportCoefficient(InitialValues, Soi
         for j = 1:ModelSettings.nD
             MN = i + j - 1;
             if ModelSettings.W_Chg == 0
-                W(i, j) = 0;
-                WW(i, j) = 0;
+                W = 0;
+                WW = 0;
                 WARG = SoilVariables.Theta_LL(i, j) * 10^7 / ModelSettings.SSUR;
                 if WARG < 80
-                    W(i, j) = Constants.W0 * exp(-WARG);
-                    WW(i, j) = Constants.W0 * exp(-WARG);
+                    W = Constants.W0 * exp(-WARG);
+                    WW = Constants.W0 * exp(-WARG);
                 end
             else
-                W(i, j) = -0.2932 * (SoilVariables.h(MN)) / 1000;
-                WW(i, j) = -0.2932 * (SoilVariables.hh(MN)) / 1000;
-                if W(i, j) > 2932
-                    W(i, j) = 2932;
+                W = -0.2932 * (SoilVariables.h(MN)) / 1000;
+                WW = -0.2932 * (SoilVariables.hh(MN)) / 1000;
+                if W > 2932
+                    W = 2932;
                 end
-                if WW(i, j) > 2932
-                    WW(i, j) = 2932;
+                if WW > 2932
+                    WW = 2932;
                 end
             end
-            f0(i, j) = SoilVariables.Theta_g(i, j)^(7 / 3) / VanGenuchten.Theta_s(i)^2;
+            f0 = SoilVariables.Theta_g(i, j)^(7 / 3) / VanGenuchten.Theta_s(i)^2;
 
             % SSUR and RHO_bulk could also be set as an array to consider more
             % than one soil type;
-            H_W(i, j) = Constants.RHOL * WW(i, j) * (SoilVariables.Theta_LL(i, j) - SoilVariables.Theta_L(i, j)) / ((ModelSettings.SSUR / Constants.RHO_bulk) * Delt_t);
+            H_W = Constants.RHOL * WW * (SoilVariables.Theta_LL(i, j) - SoilVariables.Theta_L(i, j)) / ((ModelSettings.SSUR / Constants.RHO_bulk) * Delt_t);
             if SoilVariables.TT(MN) < -20
-                MU_W(i, j) = 3.71e-2;
+                MU_W = 3.71e-2;
             elseif SoilVariables.TT(MN) > 150
-                MU_W(i, j) = 1.81e-3;
+                MU_W = 1.81e-3;
             else
-                MU_W(i, j) = Constants.MU_W0 * exp(Constants.MU1 / (8.31441 * (SoilVariables.TT(MN) + 133.3)));
+                MU_W = Constants.MU_W0 * exp(Constants.MU1 / (8.31441 * (SoilVariables.TT(MN) + 133.3)));
             end
-            L_WT(i, j) = f0(i, j) * 1e7 * 1.5550e-13 * SoilVariables.POR(i) * H_W(i, j) / (Constants.b * MU_W(i, j));
-
+            L_WT = f0 * 1e7 * 1.5550e-13 * SoilVariables.POR(i) * H_W / (Constants.b * MU_W);
             if SoilVariables.KLT_Switch == 1
-                D_Ta(i, j) = L_WT(i, j) / (Constants.RHOL * (SoilVariables.TT(MN) + 273.15));
+                D_Ta = L_WT / (Constants.RHOL * (SoilVariables.TT(MN) + 273.15));
             else
-                D_Ta(i, j) = 0;
+                D_Ta = 0;
             end
+            TransportCoefficient.W(i, j) = W;
+            TransportCoefficient.WW(i, j) = WW;
+            TransportCoefficient.D_Ta(i, j) = D_Ta;
+            TransportCoefficient.MU_W(i, j) = MU_W;
         end
     end
-    TransportCoefficient.W = W;
-    TransportCoefficient.WW = WW;
-    TransportCoefficient.D_Ta = D_Ta;
-    TransportCoefficient.MU_W = MU_W;
 end

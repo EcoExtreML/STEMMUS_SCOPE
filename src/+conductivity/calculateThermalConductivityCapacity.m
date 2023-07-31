fa03fdc0213c3eb0c2fa3e54b9f18bcbf25e4fc0
SarahAlidoost <f.alidoost@esciencecenter.nl>
2023-07-31 13:35:28 +0200

    add doc to calculateThermalConductivityCapacity

--- src/+conductivity/calculateThermalConductivityCapacity.m
+++ src/+conductivity/calculateThermalConductivityCapacity.m
@@ -1,4 +1,13 @@
 function ThermalConductivityCapacity = calculateThermalConductivityCapacity(InitialValues, ThermalConductivity, SoilVariables, VanGenuchten, DRHOVT, L, RHOV)
+    %{
+        This is to calculate thermal conductivity and thermal capacity.
+        Chung, S. O., and R. Horton (1987), Soil heat and water flow with a
+        partial surface mulch, Water Resour. Res., 23(12), 2175-2186,
+        Zeng, Y., Su, Z., Wan, L. and Wen, J.: Numerical analysis of
+        air-water-heat flow in unsaturated soil: Is it necessary to consider
+        airflow in land surface models?, J. Geophys. Res. Atmos., 116(D20),
+        20107, doi:10.1029/2011JD015835, 2011.
+    %}
 
     % get model settings
     ModelSettings = io.getModelSettings();
@@ -37,8 +46,8 @@ function ThermalConductivityCapacity = calculateThermalConductivityCapacity(Init
                 c_unsat(i, j) = EHCAP(i, j);
             else
                 MN = i + j - 1;
-                Lambda_eff(i, j) = Constants.Lambda1 + Constants.Lambda2 * Theta_LL(i, j) + Constants.Lambda3 * Theta_LL(i, j)^0.5; % 3.6*0.001*4.182; % It is possible to add the dispersion effect here to consider the heat dispersion.
-                c_unsat(i, j) = 837 * RHO_bulk / 1000 + Theta_LL(i, j) * Constants.c_L + Theta_g(i, j) * (RHODA(MN) * Constants.c_a + RHOV(MN) * Constants.c_V); % 9.79*0.1*4.182;%
+                Lambda_eff(i, j) = Constants.Lambda1 + Constants.Lambda2 * Theta_LL(i, j) + Constants.Lambda3 * Theta_LL(i, j)^0.5;
+                c_unsat(i, j) = 837 * RHO_bulk / 1000 + Theta_LL(i, j) * Constants.c_L + Theta_g(i, j) * (RHODA(MN) * Constants.c_a + RHOV(MN) * Constants.c_V);
             end
         end
     end

a19b87b2c48c26513a1639e9ad661380a891904d
Bart Schilperoort <b.schilperoort@esciencecenter.nl>
2023-11-16 08:02:19 +0100

    Split up in init/ipdate/finalize, save&load state.

--- src/STEMMUS_SCOPE_exe.m
+++ src/STEMMUS_SCOPE_exe.m
@@ -1,11 +1,14 @@
 %%%%%%% A function to run STEMMUS_SCOPE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function STEMMUS_SCOPE_exe(config_file)
+function STEMMUS_SCOPE_exe(config_file, run_mode)
 
     % disable all warnings
     warning('off', 'all');
 
-    % set the global variable CFG
+    % set the variable CFG
     CFG = config_file;
+
+    % set the variable runMode
+    runMode = run_mode;
 
     % run STEMMUS_SCOPE main code
     run STEMMUS_SCOPE;

82c15ee871634327cc53d3d1055c7599c806d3ed
Mostafa Gomaa Daoud <54531356+MostafaGomaa93@users.noreply.github.com>
2023-12-11 17:06:32 +0100

    Delete src directory

--- src/+equations/calc_rssrbs.m
+++
@@ -1,7 +0,0 @@
-function [rss, rbs] = calc_rssrbs(SoilProperties, LAI, rbs)
-
-    % an alias
-    SP = SoilProperties;
-    aa = 3.8;
-    rss = exp((aa + 4.1) - aa * (SP.SMC - SP.ResidualMC(1)) / (SP.fieldMC(1) - SP.ResidualMC(1)));
-    rbs            = rbs * LAI / 4.3;
--- src/+soilmoisture/calculatesSoilWaterFluxes.m
+++
@@ -1,9 +0,0 @@
-function HBoundaryFlux = calculatesSoilWaterFluxes(SAVE, hh)
-    %{
-        Calculate the soil water fluxes on the boundary node.
-    %}
-    ModelSettings = io.getModelSettings();
-
-    HBoundaryFlux.QMT = SAVE(2, 1, 1) - SAVE(2, 2, 1) * hh(ModelSettings.NN - 1) - SAVE(2, 3, 1) * hh(ModelSettings.NN);
-    HBoundaryFlux.QMB = -SAVE(1, 1, 1) + SAVE(1, 2, 1) * hh(1) + SAVE(1, 3, 1) * hh(2);
-end

5cbf42f9b077e883960eee341152e27ec7706fab
Bart Schilperoort <b.schilperoort@esciencecenter.nl>
2023-12-20 15:44:03 +0100

    Update BMI-mode code. Config is given w/ initialize

--- src/STEMMUS_SCOPE_exe.m
+++ src/STEMMUS_SCOPE_exe.m
@@ -1,9 +1,7 @@
 %%%%%%% A function to run STEMMUS_SCOPE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function STEMMUS_SCOPE_exe(config_file, run_mode)
-    if ~exist('run_mode', 'var')
+function STEMMUS_SCOPE_exe(config_file, runMode)
+    if ~exist('runMode', 'var')
         runMode = "full";
-    else
-        runMode = run_mode;
     end
 
     % disable all warnings
@@ -11,12 +9,13 @@ function STEMMUS_SCOPE_exe(config_file, run_mode)
 
     % set the variable CFG
     CFG = config_file;
+    bmiMode = "none";
 
     % If the runMode is "full" or was not provided, the model will run as normal
     if strcmp(runMode, "full")
         run STEMMUS_SCOPE;
 
-    elseif strcmp(runMode, "interactive")
+    elseif strcmp(runMode, "bmi")
         % In interactive mode MATLAB stays open and waits for a new command...
 
         % Define BMI required variable names:
@@ -28,20 +27,39 @@ function STEMMUS_SCOPE_exe(config_file, run_mode)
                        'TT' ... % Soil temperature over depth
                       }; %#ok
 
+        isInitialized = false;
+
         % ...until finalize has been run, at which point it quits.
-        while ~strcmp(runMode, "finalize")
-            runMode = input("\nFinished command. Select run mode: ", "s");
+        while ~strcmp(bmiMode, "finalize")
+            bmiMode = input("\nFinished command. Select run mode: ", "s");
 
-            if strcmp(runMode, "initialize") || strcmp(runMode, "update") || strcmp(runMode, "finalize")
-                % The 'initialize', 'update' and 'finalize' run modes are dispatched to the model.
+            if startsWith(bmiMode, "initialize ")
+                % Get config file:
+                CFG = eraseBetween(bmiMode, 1, 11);
+                bmiMode = "initialize";
+
                 run STEMMUS_SCOPE;
+                isInitialized = true;
 
-            elseif strcmp(runMode, "save")
+            elseif strcmp(bmiMode, "update")
+                if isInitialized
+                    % The 'initialize', 'update' and 'finalize' run modes are dispatched to the model.
+                    run STEMMUS_SCOPE;
+                else
+                    disp("First initialize the model before calling 'update'");
+                end
+
+            elseif strcmp(bmiMode, "finalize")
+                if isInitialized
+                    run STEMMUS_SCOPE;
+                end
+
+            elseif strcmp(bmiMode, "save")
                 % Save entire model state to file. This can be retrieved with the
                 %  model run mode "load", where this file is opened again.
                 save([Output_dir, 'STEMMUS_SCOPE_full_state.mat'], "-v7.3", "-nocompression");
 
-            elseif strcmp(runMode, "load")
+            elseif strcmp(bmiMode, "load")
                 % Load the full workspace file
                 [~, OutputPath, ~] = io.read_config(CFG);
                 stateFile = [OutputPath, 'STEMMUS_SCOPE_full_state.mat'];
@@ -52,12 +70,13 @@ function STEMMUS_SCOPE_exe(config_file, run_mode)
                 end
 
             else
-                % If the runMode is not one of the above, promt user again.
-                disp(["Run mode '", runMode, "' not recognised. Try again"]);
+                % If the bmiMode is not one of the above, promt user again.
+                disp(["Run mode '", bmiMode, "' not recognised. Try again"]);
             end
         end
         disp("Finished clean up. Quitting...");
 
     else
-        disp(["Run mode '", runMode, "' not recognised. The only valid modes are 'full' or 'interactive'"]);
+        disp(["Run mode '", runMode, "' not recognised. Valid modes are 'full' or 'bmi'"]);
     end
+end

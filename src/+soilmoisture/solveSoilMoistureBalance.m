f86ca9b1d823d66f0c4278c896a0cfc8feceea94
Mostafa Gomaa Daoud <54531356+MostafaGomaa93@users.noreply.github.com>
2024-02-29 08:57:15 +0100

    Update src/+soilmoisture/solveSoilMoistureBalance.m
    Co-authored-by: SarahAlidoost <55081872+SarahAlidoost@users.noreply.github.com>

--- src/+soilmoisture/solveSoilMoistureBalance.m
+++ src/+soilmoisture/solveSoilMoistureBalance.m
@@ -24,11 +24,7 @@ function [SoilVariables, HeatMatrices, HeatVariables, HBoundaryFlux, Rn_SOIL, Ev
         Trap = [];
         r_a_SOIL = InitialValues.r_a_SOIL;
     end
-    
-    GroundwaterSettings = io.readGroundwaterSettings() % added by Mostafa
-    headBotmLayer = GroundwaterSettings.headBotmLayer; % added by Mostafa
-    indexBotmLayer = GroundwaterSettings.indexBotmLayer; % added by Mostafa
-   
+  
     [AVAIL0, RHS, HeatMatrices, Precip] = soilmoisture.calculateBoundaryConditions(BoundaryCondition, HeatMatrices, ForcingData, SoilVariables, InitialValues, ...
                                                                                    TimeProperties, SoilProperties, RHS, hN, KT, Delt_t, Evap);
 

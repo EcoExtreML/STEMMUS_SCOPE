f7919de5b3e5d01a5405bc0e4ebd2cf7f79afddf
Mostafa Gomaa Daoud <54531356+MostafaGomaa93@users.noreply.github.com>
2024-06-19 10:58:35 +0200

    Update solveSoilMoistureBalance.m

--- src/+soilmoisture/solveSoilMoistureBalance.m
+++ src/+soilmoisture/solveSoilMoistureBalance.m
@@ -36,10 +36,11 @@ function [SoilVariables, HeatMatrices, HeatVariables, HBoundaryFlux, Rn_SOIL, Ev
     % fix hh values
     ModelSettings = io.getModelSettings();
 
-    if ~GroundwaterSettings.GroundwaterCoupling  % Groundwater Coupling is not activated, added by Mostafa
-        indxBotm = 1; % index of bottom layer, by default (no groundwater coupling) its layer with index 1, since STEMMUS calculations starts from bottom to top
-    else % Groundwater Coupling is activated, added by Mostafa
-        indxBotm = GroundwaterSettings.indxBotmLayer; % index of bottom boundary layer after neglecting the saturated layers (from bottom to top)
+    if ~GroundwaterSettings.GroundwaterCoupling  % no Groundwater coupling, added by Mostafa
+        indxBotm = 1; % index of bottom layer is 1, STEMMUS calculates from bottom to top
+    else % Groundwater Coupling is activated
+        % index of bottom layer after neglecting saturated layers (from bottom to top)           
+        indxBotm = GroundwaterSettings.indxBotmLayer;
     end
 
     for i = indxBotm:ModelSettings.NN

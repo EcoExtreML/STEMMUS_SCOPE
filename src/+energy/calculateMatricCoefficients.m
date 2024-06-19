e9c407697a5498f79d0a4c58320229973192e2ea
Mostafa Gomaa Daoud <54531356+MostafaGomaa93@users.noreply.github.com>
2024-06-19 10:16:45 +0200

    Update calculateMatricCoefficients.m

--- src/+energy/calculateMatricCoefficients.m
+++ src/+energy/calculateMatricCoefficients.m
@@ -14,10 +14,11 @@ function EnergyMatrices = calculateMatricCoefficients(EnergyVariables, InitialVa
     EnergyMatrices.C6 = InitialValues.C6;
     EnergyMatrices.C7 = zeros(ModelSettings.NN);
 
-    if ~GroundwaterSettings.GroundwaterCoupling  % Groundwater Coupling is not activated, added by Mostafa
-        indxBotm = 1; % index of bottom layer, by defualt (no groundwater coupling) its layer with index 1, since STEMMUS calcuations starts from bottom to top
-    else % Groundwater Coupling is activated, added by Mostafa
-        indxBotm = GroundwaterSettings.indxBotmLayer; % index of bottom boundary layer after neglecting the saturated layers (from bottom to top)
+    if ~GroundwaterSettings.GroundwaterCoupling  % no Groundwater coupling, added by Mostafa
+        indxBotm = 1; % index of bottom layer is 1, STEMMUS calculates from bottom to top
+    else % Groundwater Coupling is activated
+        % index of bottom layer after neglecting saturated layers (from bottom to top)           
+        indxBotm = GroundwaterSettings.indxBotmLayer;
     end
 
     for i = indxBotm:ModelSettings.NL

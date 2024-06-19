9937d58e447c6156c77ed069f7bedb0b0bdc1504
Mostafa Gomaa Daoud <54531356+MostafaGomaa93@users.noreply.github.com>
2024-06-19 09:51:10 +0200

    Update calculateBoundaryConditions.m

--- src/+energy/calculateBoundaryConditions.m
+++ src/+energy/calculateBoundaryConditions.m
@@ -10,11 +10,12 @@ function [RHS, EnergyMatrices] = calculateBoundaryConditions(BoundaryCondition, 
 
     Constants = io.define_constants();
 
-    if ~GroundwaterSettings.GroundwaterCoupling  % Groundwater Coupling is not activated, added by Mostafa
-        indxBotm = 1; % index of bottom layer, by defualt (no groundwater coupling) its layer with index 1, since STEMMUS calcuations starts from bottom to top
+    if ~GroundwaterSettings.GroundwaterCoupling  % no Groundwater coupling, added by Mostafa
+        indxBotm = 1; % index of bottom layer is 1, STEMMUS calculates from bottom to top
         tempBotm = BoundaryCondition.BCTB;
     else % Groundwater Coupling is activated
-        indxBotm = GroundwaterSettings.indxBotmLayer; % index of bottom boundary layer after neglecting the saturated layers (from bottom to top)
+        % index of bottom layer after neglecting saturated layers (from bottom to top)
+        indxBotm = GroundwaterSettings.indxBotmLayer;
         tempBotm = GroundwaterSettings.tempBotm; % groundwater temperature
     end
 

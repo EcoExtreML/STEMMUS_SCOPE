795ba07e044a8374baabe1b565b64ea1d0d0114a
Mostafa Gomaa Daoud <54531356+MostafaGomaa93@users.noreply.github.com>
2024-06-19 11:13:09 +0200

    Update src/+groundwater/findPhreaticSurface.m
    Co-authored-by: SarahAlidoost <55081872+SarahAlidoost@users.noreply.github.com>

--- src/+groundwater/findPhreaticSurface.m
+++ src/+groundwater/findPhreaticSurface.m
@@ -32,7 +32,6 @@ function [depToGWT, indxGWLay] = findPhreaticSurface(SoilVariables, KT, Groundwa
     % Load Groundwater settings
     if GroundwaterSettings.GroundwaterCoupling == 1 % Groundwater coupling is enabled
         soilThick = GroundwaterSettings.soilThick;
-        indxBotmLayer = GroundwaterSettings.indxBotmLayer;
         indxBotmLayer_R = GroundwaterSettings.indxBotmLayer_R;
 
         % Call the matric potential

bc2d2224a0cd5d203c089f1afd0b8adb122cbb8a
Mostafa Gomaa Daoud <54531356+MostafaGomaa93@users.noreply.github.com>
2024-06-19 12:15:10 +0200

    Update calc_rsoil.m

--- src/calc_rsoil.m
+++ src/calc_rsoil.m
@@ -2,7 +2,6 @@ function [PSIs, rsss, rrr, rxx] = calc_rsoil(Rl, ModelSettings, SoilVariables, V
     % load Constants
     Constants = io.define_constants();
 
-    DeltZ0 = ModelSettings.DeltZ';
     SMC = SoilVariables.Theta_LL(1:54, 2);
     Se  = (SMC - VanGenuchten.Theta_r') ./ (VanGenuchten.Theta_s' - VanGenuchten.Theta_r');
     Ksoil = SoilVariables.Ks' .* Se.^Constants.l .* (1 - (1 - Se.^(1 ./ m')).^(m')).^2;
@@ -18,6 +17,6 @@ function [PSIs, rsss, rrr, rxx] = calc_rsoil(Rl, ModelSettings, SoilVariables, V
         PSIs = hh_lay / 100 .* bbx; % unit conversion from cm to m (needed in the ebal calculations)
     end
 
-    rsss = 1 ./ Ksoil ./ Rl ./ DeltZ0 / 2 / pi .* log((pi * Rl).^(-0.5) / (0.5 * 1e-3)) * 100 .* bbx;
-    rxx  = 1 * 1e10 * DeltZ0 / 0.5 / 0.22 ./ Rl / 100 .* bbx; % Delta_z*j is the depth of the layer
-    rrr  = 4 * 1e11 * (VanGenuchten.Theta_s' ./ SMC) ./ Rl ./ (DeltZ0 / 100) .* bbx;
+    rsss = 1 ./ Ksoil ./ Rl ./ ModelSettings.DeltZ' / 2 / pi .* log((pi * Rl).^(-0.5) / (0.5 * 1e-3)) * 100 .* bbx;
+    rxx  = 1 * 1e10 * ModelSettings.DeltZ' / 0.5 / 0.22 ./ Rl / 100 .* bbx; % Delta_z*j is the depth of the layer
+    rrr  = 4 * 1e11 * (VanGenuchten.Theta_s' ./ SMC) ./ Rl ./ (ModelSettings.DeltZ' / 100) .* bbx;

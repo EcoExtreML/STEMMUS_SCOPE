cef58585e3f394f4ef7a96d0f351f48636afedd9
Mostafa Gomaa Daoud <54531356+MostafaGomaa93@users.noreply.github.com>
2024-07-10 14:28:27 +0200

    Change function name and update Precip_msr

--- src/+groundwater/updateRunoff.m
+++ src/+groundwater/updateRunoff.m
@@ -1,13 +1,15 @@
-function dunnianRunoff = updateRunoff(Precip_msr, groundWaterDepth)
-    % Different approach (not CLM). Dunnian runoff = Direct water input from precipitation + return flow
+function [dunnianRunoff, Precip_msr] = updateDunnianRunoff(ForcingData, groundWaterDepth)
+    % Dunnian runoff = Direct water input from precipitation + return flow
     % (a) direct water input from precipitation when soil is fully saturated (depth to water table = 0)
     % (b) Return flow (from groundwater exfiltration) calculated in MODFLOW and added to Dunnian runoff (through BMI)
     % here approach (a) is implemented
     wat_Dep = groundWaterDepth / 100; % (m);
     if wat_Dep <= 0.01
-        dunnianRunoff = Precip_msr;
+        dunnianRunoff = ForcingData.Precip_msr;
+        Precip_msr = ForcingData.Precip_msr .* 0;		
     else
-        dunnianRunoff = zeros(size(Precip_msr));
+        dunnianRunoff = zeros(size(ForcingData.Precip_msr));
+        Precip_msr = ForcingData.Precip_msr;		
     end
 
 end

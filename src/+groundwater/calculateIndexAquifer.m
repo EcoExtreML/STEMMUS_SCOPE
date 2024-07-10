eb790ed45a92ea06d4f748d21a576f9e2358803d
Mostafa Gomaa Daoud <54531356+MostafaGomaa93@users.noreply.github.com>
2024-07-10 14:11:15 +0200

    add soilThick as an input and add a line to define the ModelSettings

--- src/+groundwater/calculateIndexAquifer.m
+++ src/+groundwater/calculateIndexAquifer.m
@@ -1,15 +1,16 @@
-function indxAqLay = calculateIndexAquifer(aqLayers, numAqL)
+function indxAqLay = calculateIndexAquifer(aqLayers, numAqL, soilThick)
     %{
-        Assign the index of the MODFLOW aquifer that corresponds to each STEMMUS
-        soil layer.
+        Assign the index of the MODFLOW aquifer that corresponds to each STEMMUS soil layer.
         aqLayers            elevation of top surface level and all bottom levels of aquifer layers, received from MODFLOW through BMI
         numAqL              number of MODFLOW aquifer layers, received from MODFLOW through BMI
     %}
 
+    ModelSettings = io.getModelSettings();
+
     numAqN = numAqL + 1; % number of MODFLOW aquifer nodes
-    indxAqLay = zeros(NN, 1);
+    indxAqLay = zeros(ModelSettings.NN, 1);
     indxAqLay(1) = 1;
-    for i = 2:NN
+    for i = 2:ModelSettings.NN
         for K = 2:numAqN
             Z1 = aqLayers(K - 1);
             Z0 = aqLayers(K);

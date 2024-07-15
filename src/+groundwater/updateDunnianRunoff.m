b2e3df919e35da76cf6526d8efff0acdc546477c
SarahAlidoost <55081872+SarahAlidoost@users.noreply.github.com>
2024-07-15 11:14:21 +0200

    remove conversion of waterDepth
    Co-authored-by: Bart Schilperoort <b.schilperoort@esciencecenter.nl>

--- src/+groundwater/updateDunnianRunoff.m
+++ src/+groundwater/updateDunnianRunoff.m
@@ -3,12 +3,10 @@ function [dunnianRunoff, update_Precip_msr] = updateDunnianRunoff(Precip_msr, gr
     % (a) direct water input from precipitation when soil is fully saturated (depth to water table = 0)
     % (b) Return flow (from groundwater exfiltration) calculated in MODFLOW and added to Dunnian runoff (through BMI)
     % here approach (a) is implemented
-    wat_Dep = groundWaterDepth / 100; % (m);
-
     dunnianRunoff = zeros(size(Precip_msr));
     update_Precip_msr = Precip_msr;
 
-    if wat_Dep <= 0.01
+    if groundWaterDepth <= 1.0
         dunnianRunoff = Precip_msr;
         update_Precip_msr = zeros(size(Precip_msr));
     end

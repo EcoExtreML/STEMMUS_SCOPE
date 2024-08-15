fbb938b029191da63259829360bcf03d113d0adf
Mostafa Gomaa Daoud <54531356+MostafaGomaa93@users.noreply.github.com>
2024-08-15 11:26:54 +0200

    Update readSoilLayerSettings.m

--- src/+io/readSoilLayerSettings.m
+++ src/+io/readSoilLayerSettings.m
@@ -3,12 +3,14 @@ function SoilLayerSettings = readSoilLayerSettings(soilLayersFile)
 
     SoilLayerSettings.NL = soildata(end, 1);
     SoilLayerSettings.ML = SoilLayerSettings.NL;
+    SoilLayerSettings.NN = SoilLayerSettings.NL + 1;
+    SoilLayerSettings.mN = SoilLayerSettings.NN + 1;
+    SoilLayerSettings.mL = SoilLayerSettings.NL + 1;
+    SoilLayerSettings.nD = 2;
 
     SoilLayerSettings.DeltZ_R = transpose(soildata(:, 2));
     SoilLayerSettings.DeltZ = flip(SoilLayerSettings.DeltZ_R);
 
     SoilLayerSettings.Tot_Depth = sum(SoilLayerSettings.DeltZ_R);
-
     SoilLayerSettings.R_depth = soildata(1, 3);
-
 end
